40560b1a65e1efc57b63c1c1d26235743cc4949a
Gemeiii <gemei95@hotmail.com>
2017-02-20 16:42:34 -0500

    Update BasicAnimal.java

--- AnimalProgram/Animals/BasicAnimal.java
+++ AnimalProgram/Animals/BasicAnimal.java
@@ -1,10 +1,10 @@
-package Animals;
+package Animals;                         // package name (same as folder name) apply to other files in Animal folder, especially inheritance
 
-public class BasicAnimal {
-  public String sound;
+public class BasicAnimal {               // class, pascal
+  public String sound;                   // member variable, camel
 
   public BasicAnimal(){
-    this.sound = "Generic animal noise";
+    this.sound = "Generic animal noise";  // define variable sound, = default
   }
 
   public void makeNoise(){
