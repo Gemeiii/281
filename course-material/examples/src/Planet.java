3c869123024e4d808081bc53eac359bc871167b2
Gemeiii <gemei95@hotmail.com>
2017-04-10 11:14:09 -0400

    added notes

--- course-material/examples/src/Planet.java
+++ course-material/examples/src/Planet.java
@@ -8,17 +8,17 @@ public enum Planet {
   URANUS(8.686e+25, 2.5559e7), 
   NEPTUNE(1.024e+26, 2.4746e7);
 
-  private final double mass; // in kilograms
+  private final double mass; // in kilograms     //只在这一个class里面访问
   private final double radius; // in meters
   
-  public static final String GALAXY = "Milky Way";
+  public static final String GALAXY = "Milky Way";   //其他
 
-  Planet(double mass, double radius) {
-    this.mass = mass;
+  Planet(double mass, double radius) {   //构造函数
+    this.mass = mass; //第一个是11行的，用this的话指的是这个对象， 第二个是16行的。
     this.radius = radius;
   }
 
-  private double mass() {
+  private double mass() {   //method 
     return mass;
   }
 
@@ -27,25 +27,25 @@ public enum Planet {
   }
 
   // universal gravitational constant (m3 kg-1 s-2)
-  public static final double G = 6.67300E-11;
+  public static final double G = 6.67300E-11;    // 要给type
 
-  double surfaceGravity() {
+  double surfaceGravity() {      //同一个package里面可以访问方法或者变量  //method 
     return G * mass / (radius * radius);
   }
 
-  double surfaceWeight(double otherMass) {
+  double surfaceWeight(double otherMass) {   // otherMass is just random name 
     return otherMass * surfaceGravity();
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) {    //入口函数  //先来找这个
     if (args.length != 1) {
       System.err.println("Usage: java Planet <earth_weight>");
-      System.exit(-1);
-    }
-    double earthWeight = Double.parseDouble(args[0]);
-    double mass = earthWeight / EARTH.surfaceGravity();
-    for (Planet p : Planet.values()) {
-      System.out.printf("Your weight on %s is %f%n", p, p.surfaceWeight(mass));
+      System.exit(-1);   // 就是退出的意思
+    }    //如果＝1 就会直接往下进行
+    double earthWeight = Double.parseDouble(args[0]); //Double:是一个class，java自己带的。
+    double mass = earthWeight / EARTH.surfaceGravity();  //用的是earth的数字
+    for (Planet p : Planet.values()) {                    //从0开始，".values()"转换成list
+      System.out.printf("Your weight on %s is %f%n", p, p.surfaceWeight(mass));  //格式
     }
     System.out.println("All of these planets are in the "+ Planet.GALAXY + " galaxy.");
   }
