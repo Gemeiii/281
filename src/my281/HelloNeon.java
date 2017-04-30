8737c2a1c6937bb1fe44c6fa0b8435dea8442a82
Gemeiii <Gemei@mac-0.local>
2017-04-30 13:53:07 -0400

    added sorting example

---
+++ src/my281/HelloNeon.java
@@ -0,0 +1,14 @@
+package my281;
+
+public class HelloNeon {
+  public static String hello = "hey";
+
+  public static void main(String[] args){
+	 int count=3;
+	 while(count<2){
+	    System.out.println(hello);
+	    //++count;
+      }
+	  
+  }
+}  
